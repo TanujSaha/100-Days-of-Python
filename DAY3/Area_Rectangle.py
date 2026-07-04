7dd53373f9679bf4285d306fbcca6de5dfac9491
TanujSaha <Sahatanuj41@gmail.com>
2026-07-05 00:58:55 +0530

    Day 3: Basic easy problems

---
+++ DAY3/Area_Rectangle.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the Rectangle:"))
+width = float(input("Enter the width of the Rectangle:"))
+
+area = length * width
+
+print("Area of the Rectangle is:", area)
